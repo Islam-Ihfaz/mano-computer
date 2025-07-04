3f3fa4a01563703d0c71ef9b3358bc8ed0fdc2fe
Islam-Ihfaz <144639167+Islam-Ihfaz@users.noreply.github.com>
2025-07-03 21:35:40 -0400

    Update OpDecoder.v

--- src/OpDecoder.v
+++ src/OpDecoder.v
@@ -1,7 +1,7 @@
 module OpDecoder(ir0, ir1, q1, q2, q3);
-input ir0, ir1;
-output q1, q2, q3;
-wire q1 = ir0 & !ir1,
-q2 = ir1 & !ir0,
-q3 = ir0 & ir1;
+  input ir0, ir1;
+  output q1, q2, q3;
+  wire q1 = ir0 & !ir1,
+       q2 = ir1 & !ir0,
+       q3 = ir0 & ir1;
 endmodule
