e4ff87bba98909b29434a598ba245332cc1322f0
Islam-Ihfaz <144639167+Islam-Ihfaz@users.noreply.github.com>
2025-07-03 21:37:56 -0400

    Update TimeDecoder.v

--- src/TimeDecoder.v
+++ src/TimeDecoder.v
@@ -1,13 +1,14 @@
 module TimeDecoder(t, t0, t1, t2, t3, t4, t5, t6, t7);
-output t0, t1, t2, t3, t4, t5, t6, t7;
-input wire [2:0] t;
+  output t0, t1, t2, t3, t4, t5, t6, t7;
+  input wire [2:0] t;
+
 assign
-t0 = ~t[2] & ~t[1] & ~t[0],
-t1 = ~t[2] & ~t[1] & t[0],
-t2 = ~t[2] & t[1] & ~t[0],
-t3 = ~t[2] & t[1] & t[0],
-t4 = t[2] & ~t[1] & ~t[0],
-t5 = t[2] & ~t[1] & t[0],
-t6 = t[2] & t[1] & ~t[0],
-t7 = t[2] & t[1] & t[0];
+  t0 = ~t[2] & ~t[1] & ~t[0],
+  t1 = ~t[2] & ~t[1] & t[0],
+  t2 = ~t[2] & t[1] & ~t[0],
+  t3 = ~t[2] & t[1] & t[0],
+  t4 = t[2] & ~t[1] & ~t[0],
+  t5 = t[2] & ~t[1] & t[0],
+  t6 = t[2] & t[1] & ~t[0],
+  t7 = t[2] & t[1] & t[0];
 endmodule
